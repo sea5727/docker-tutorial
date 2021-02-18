981fa18c3c21e1bd96e83b95db3970255c20cbe7
ysh8361 <sea5727@naver.com>
2021-02-19 01:43:17 +0900

    commit

---
+++ docker-cetnos-7/README.md
@@ -0,0 +1,11 @@
+# centos7
+
+커스텀 ceotos 7
+
+hostname을 변경하기 위해서는
+
+-h 호스트네임
+```
+$ docker run -i -t -d -h MYTIBERO --name centos7 sea5727/centos7:dev /bin/bash 
+```
+
---
+++ docker-tibero/tmp/create_db.sql
@@ -0,0 +1,30 @@
+create database "tibero"
+	user sys identified by tibero
+	maxinstances 8
+	maxdatafiles 100
+	character set MSWIN949
+	logfile group 1 'log001.log' 
+			size 100M,
+		group 2 'log002.log' 
+			size 100M,
+		group 3 'log003.log' 
+			size 100M
+	maxloggroups 255
+	maxlogmembers 8
+	noarchivelog
+		datafile 'system001.dtf' 
+			size 100M
+			autoextend on next 100M
+			maxsize unlimited
+	default temporary tablespace TEMP
+		tempfile 'temp001.dtf' 
+			size 100M
+			autoextend on next 100M
+			maxsize unlimited
+			extent management local autoallocate
+	undo tablespace UNDO
+		datafile 'undo001.dtf' 
+			size 100M
+			autoextend on next 100M
+			maxsize unlimited
+			extent management local autoallocate;
